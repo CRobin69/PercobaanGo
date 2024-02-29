cfd4c4a8d691352e0cebbb31a155128aa02c1954
Christopher Robin <williamchen1506@gmail.com>
2024-02-29 16:53:30 +0700

    Pengenalan API menggunakan GIN & GORM

---
+++ migrate/migrate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"API/initializers"
+	"API/models"
+)
+
+func init() {
+	initializers.LoadEnvVariabels()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.Post{})
+}
